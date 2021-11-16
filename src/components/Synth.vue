7f5fa0c1b86eeb8b8a238834215fd22bebd4b31c
yann <yann.boisselier@gmail.com>
2021-11-16 21:47:50 +0100

    compose useOscillatorWithKeyboard

--- src/components/Synth.vue
+++ src/components/Synth.vue
@@ -1,94 +1,54 @@
 <template>
-  <h2>Play with your keyboard: azertyuiopqsdfghjklmwxcvbn</h2>
+  <h2>Presser une lettre du clavier pour jouer</h2>
+  <p>{{ oscillatorA.state }}</p>
+  <div>
+    <h2>Oscillateur A</h2>
+    <p>Pitch</p>
+    <input @input="handlePitchA" type="range" min="55" max="220" />
 
-  <p>Pitch oscillateur A</p>
-  <input
-    @input="handlePitch"
-    type="range"
-    id="frequence"
-    name="frequence"
-    min="55"
-    max="220"
-  />
+    <div>
+      <p>Forme d'onde</p>
+      <select v-model="oscillatorA.state.waveForm">
+        <option value="sine">Sine</option>
+        <option value="square">Square</option>
+        <option value="triangle">Triangle</option>
+      </select>
+    </div>
 
-  <select v-model="oscillatorWaveForm">
-    <option value="sine">Sine</option>
-    <option value="square">Square</option>
-    <option value="triangle">Triangle</option>
-  </select>
-
-  <p v-show="currentFrequence">{{ currentFrequence }} Hz</p>
+    <div>
+      <p>Volume ({{ oscillatorA.state.gain * 100 }} %)</p>
+      <input
+        @input="handleGainA"
+        type="range"
+        min="0"
+        max="100"
+        :value="oscillatorA.state.gain * 100"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from "vue";
-
-function generateNotes(startNote = 55) {
-  // https://lecompositeur.com/wp-content/uploads/2016/04/Frequences.pdf
-  // Fréquence = 440 Hz * r puissance n
-  // où n est le nombre de demi-tons
-  // fréquence do 4 = 440 * r puissance 3
-  // résultat: 523.251 Hz
-  const notes = new Map();
-  const r = 1.05946;
-  const keys = "azertyuiopqsdfghjklmwxcvbn";
-  for (let i = 0; i < keys.length; i++) {
-    const frequence = startNote * Math.pow(r, i + 1);
-    notes.set(keys[i], frequence);
-  }
-  return notes;
-}
+import { defineComponent } from "vue";
+import { useOscillatorWithKeyboard } from "@/composables/useSynth";
 
 export default defineComponent({
   setup() {
-    const currentFrequence = ref(null as null | number);
-    const notes = ref(generateNotes());
-    // eslint-disable-next-line
-    const oscillatorWaveForm = ref("sine" as OscillatorType);
-
     const audioContext = new AudioContext();
-    const oscillatorAudioNode = audioContext.createOscillator();
-    const gainAudioNode = audioContext.createGain();
-    gainAudioNode.connect(audioContext.destination);
+    const oscillatorA = useOscillatorWithKeyboard(audioContext);
 
-    let oscillatorStarted = false;
-
-    oscillatorAudioNode.frequency.value = 100;
-    oscillatorAudioNode.type = oscillatorWaveForm.value;
-
-    watch(oscillatorWaveForm, (v) => {
-      oscillatorAudioNode.type = v;
-    });
-
-    document.addEventListener("keydown", (e) => {
-      // Note: un oscillateur ne peut être démarré qu'une seule fois.
-      // Et on ne peut pas le démarrer sans interaction utilisateur car Chrome interdit cela
-      // On joue juste la connexion / déconnexion à la sortie audio pour le faire démarrer / arrêter
-      if (oscillatorStarted === false) {
-        oscillatorStarted = true;
-        oscillatorAudioNode.start();
-      }
-      if (notes.value.get(e.key)) {
-        oscillatorAudioNode.connect(gainAudioNode);
-        currentFrequence.value = notes.value.get(e.key);
-        oscillatorAudioNode.frequency.value = notes.value.get(e.key);
-      }
-    });
-
-    document.addEventListener("keyup", (e) => {
-      if (notes.value.get(e.key)) {
-        oscillatorAudioNode.disconnect(gainAudioNode);
-      }
-    });
-
-    function handlePitch(event: any) {
-      notes.value = generateNotes(event.target.value);
+    function handlePitchA(event: any) {
+      oscillatorA.state.pitch = event.target.value;
+    }
+    function handleGainA(event: any) {
+      console.log("event.target.value ", event.target.value);
+      oscillatorA.state.gain = event.target.value / 100;
     }
 
     return {
-      currentFrequence,
-      handlePitch,
-      oscillatorWaveForm,
+      oscillatorA,
+      handlePitchA,
+      handleGainA,
     };
   },
 });
---
+++ src/composables/useSynth.ts
@@ -0,0 +1,114 @@
+import { reactive, watch, ref } from "vue";
+
+interface State {
+  waveForm: OscillatorType;
+  gain: number;
+  pitch: number;
+  currentFrequence: undefined | number;
+}
+
+interface useOscillatorReturn {
+  oscillatorNode: OscillatorNode;
+  gainNode: GainNode;
+  state: State;
+}
+
+/**
+ * An oscillator has:
+ * - a reactive gain (from 0 to 1)
+ * - a reactive waveform
+ */
+export function useOscillatorWithKeyboard(
+  audioContext: AudioContext
+): useOscillatorReturn {
+  let oscillatorStarted = false;
+  const state = reactive<State>({
+    waveForm: "sine",
+    gain: 1,
+    pitch: 55,
+    currentFrequence: undefined,
+  });
+
+  // Connect oscillator to gain, connect gain to audio output.
+  const oscillatorNode = audioContext.createOscillator();
+  const gainNode = audioContext.createGain();
+  gainNode.connect(audioContext.destination);
+
+  const notes = ref(keyboardMap(state.pitch));
+
+  oscillatorNode.type = state.waveForm;
+
+  watch(
+    () => state.waveForm,
+    (value: OscillatorType) => {
+      oscillatorNode.type = value;
+    }
+  );
+
+  watch(
+    () => state.pitch,
+    (value: number) => {
+      notes.value = keyboardMap(value);
+    }
+  );
+
+  watch(
+    () => state.gain,
+    (value: number) => {
+      if (value < 0 || value > 1) {
+        throw new Error(
+          "useOscillator: Le gain doit être compris entre 0 et 1"
+        );
+      }
+      gainNode.gain.value = value;
+    }
+  );
+
+  document.addEventListener("keydown", (e) => {
+    if (notes.value.get(e.key)) {
+      // Note: un oscillateur ne peut être démarré qu'une seule fois.
+      // Et on ne peut pas le démarrer sans interaction utilisateur car Chrome interdit cela
+      // On joue juste la connexion / déconnexion à la sortie audio pour le faire démarrer / arrêter
+      if (oscillatorStarted === false) {
+        oscillatorStarted = true;
+        oscillatorNode.start();
+      }
+      oscillatorNode.connect(gainNode);
+      state.currentFrequence = notes.value.get(e.key);
+      if (state.currentFrequence) {
+        oscillatorNode.frequency.value = state.currentFrequence;
+      }
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (notes.value.get(e.key)) {
+      oscillatorNode.disconnect(gainNode);
+    }
+  });
+
+  return {
+    oscillatorNode,
+    gainNode,
+    state,
+  };
+}
+
+// fonction d'aide pour savoir quelle fréquence l'oscillateur
+// doit avoir en fonction d'une touche du clavier.
+// @param startNode: Fréquence en Hs de la note de départ qui permet de calculer toutes les autres
+function keyboardMap(startNote = 55): Map<string, number> {
+  // https://lecompositeur.com/wp-content/uploads/2016/04/Frequences.pdf
+  // Fréquence = 440 Hz * r puissance n
+  // où n est le nombre de demi-tons
+  // fréquence do 4 = 440 * r puissance 3
+  // résultat: 523.251 Hz
+  const notes = new Map();
+  const r = 1.05946;
+  const keys = "azertyuiopqsdfghjklmwxcvbn";
+  for (let i = 0; i < keys.length; i++) {
+    const frequence = startNote * Math.pow(r, i + 1);
+    notes.set(keys[i], frequence);
+  }
+  return notes;
+}
