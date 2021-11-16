2f1f85827b5a4997ac46e3c299e9eed471c4b370
yann <yann.boisselier@gmail.com>
2021-11-16 15:45:32 +0100

    play with keys aqzsedrftgyhujikolpm

--- src/components/Synth.vue
+++ src/components/Synth.vue
@@ -1,15 +1,16 @@
 <template>
-  <h2>Oscillator 1</h2>
+  <h2>Play with keys: aqzsedrftgyhujikolpm</h2>
+  <p v-show="currentFrequence">{{ currentFrequence }} Hz</p>
+  <!--
   <input
     @input="handleFrequence"
     type="range"
     id="frequence"
     name="frequence"
     min="50"
-    max="10000"
+    max="200"
   />
-  <button @click="startOscillator" :disabled="!oscillatorMuted">Start</button>
-  <button @click="stopOscillator" :disabled="oscillatorMuted">Mute</button>
+  -->
 </template>
 
 <script lang="ts">
@@ -18,6 +19,7 @@ import { defineComponent, ref } from "vue";
 export default defineComponent({
   setup() {
     const oscillatorMuted = ref(true);
+    const currentFrequence = ref(null as null | number);
     const audioContext = new AudioContext();
     const oscillatorAudioNode = audioContext.createOscillator();
     const gainAudioNode = audioContext.createGain();
@@ -26,8 +28,52 @@ export default defineComponent({
 
     let oscillatorStarted = false;
 
+    const notes = new Map();
+    // do
+    notes.set("a", 311.126);
+    notes.set("q", 329.627);
+    // ré
+    notes.set("z", 349.228);
+    notes.set("s", 369.994);
+    notes.set("e", 391.995);
+    notes.set("d", 415.304);
+    // la
+    notes.set("r", 440);
+    notes.set("f", 466.163);
+    notes.set("t", 493.883);
+    notes.set("g", 523.251);
+    notes.set("y", 554.365);
+    notes.set("h", 587.329);
+    notes.set("u", 622.253);
+    notes.set("j", 659.255);
+    notes.set("i", 698.456);
+    notes.set("k", 739.988);
+    notes.set("o", 783.991);
+    notes.set("l", 830.609);
+    notes.set("p", 880.0);
+    notes.set("m", 932.327);
+
     oscillatorAudioNode.frequency.value = 100;
     oscillatorAudioNode.type = "square";
+
+    document.addEventListener("keydown", (e) => {
+      if (oscillatorStarted === false) {
+        oscillatorStarted = true;
+        oscillatorAudioNode.start();
+      }
+      if (notes.get(e.key)) {
+        oscillatorAudioNode.connect(gainAudioNode);
+        currentFrequence.value = notes.get(e.key);
+        oscillatorAudioNode.frequency.value = notes.get(e.key);
+      }
+    });
+
+    document.addEventListener("keyup", (e) => {
+      if (notes.get(e.key)) {
+        oscillatorAudioNode.disconnect(gainAudioNode);
+        console.log("e", e.key);
+      }
+    });
 
     // un oscillateur ne peut être démarrer qu'une seule fois.
     // Et on ne peut pas le démarrer sans interaction utilisateur car Chrome interdit cela
@@ -45,12 +91,13 @@ export default defineComponent({
       oscillatorAudioNode.disconnect(gainAudioNode);
     }
 
-    function handleFrequence(value) {
-      oscillatorAudioNode.frequency.value = value.target.value;
-      console.log(value.target.value);
+    function handleFrequence(event: any) {
+      oscillatorAudioNode.frequency.value = event.target.value;
+      console.log(event.target.value);
     }
 
     return {
+      currentFrequence,
       startOscillator,
       stopOscillator,
       oscillatorMuted,
